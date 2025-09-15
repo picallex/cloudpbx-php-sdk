b771b2f93dcf76fe84663a5a9c6bc9e5f203102c
Matias Gomez <matiias.gomeez999@gmail.com>
2025-09-15 16:54:54 -0300

    add new model for status did

---
+++ src/Cloudpbx/Sdk/Model/Customer/StatusReportDid.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cloudpbx\Sdk\Model\Customer;
+
+final class StatusReportDid extends \Cloudpbx\Sdk\Model
+{
+    private const DEFAULT_ID = 0;
+
+    /** @var int */
+    public $id;
+
+    /** @var array */
+    public $callerid_info = [];
+
+    /** @var int */
+    public $customer_id;
+
+    /** @var object */
+    public $did_destination;
+
+    /** @var object */
+    public $follow_me;
+
+    /** @var array */
+    public $follow_me_entries = [];
+
+    public function __construct($data = [])
+    {
+        $this->callerid_info = $data['callerid_info'] ?? [];
+        $this->customer_id = (int)($data['customer_id'] ?? self::DEFAULT_ID);
+        $this->did_destination = (object)($data['did_destination'] ?? []);
+        $this->id = (int)($data['did_id'] ?? self::DEFAULT_ID);
+        $this->follow_me = (object)($data['follow_me'] ?? []);
+        $this->follow_me_entries = $data['follow_me_entries'] ?? [];
+    }
+}
