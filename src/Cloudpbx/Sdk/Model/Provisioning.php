1d73eebe2a4ef0c42f5c4924a0d593bcf86da93c
agusserraalh <agustin@picallex.com>
2026-05-05 14:46:15 -0300

    feat: fix name

--- src/Cloudpbx/Sdk/Model/Provisioning.php
+++ src/Cloudpbx/Sdk/Model/Provisioning.php
@@ -3,7 +3,7 @@
 // This file is part of cloudpbx-php-sdk.  The COPYRIGHT file at the top level of
 // this repository contains the full copyright notices and license terms.
 //
-// @author (2021) Jovany Leandro G.C <jovany@picallex.com>
+// @author (2026) Agustin Serra <agustin@picallex.com>
 
 declare(strict_types=1);
 
